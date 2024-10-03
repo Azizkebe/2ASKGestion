c8823ce853a50b62930ca5cf22b620011ae8e2e5
Azizkebe <kebeziza123@gmail.com>
2024-10-03 11:49:23 +0000

    Partie 2

--- gestionrh/app/Livewire/ListeYear.php
+++ gestionrh/app/Livewire/ListeYear.php
@@ -8,6 +8,7 @@ use App\Models\ParamTypeConge;
 use App\Models\PermissionConge;
 use App\Models\Employe;
 use Carbon\Carbon;
+use Auth;
 
 class ListeYear extends Component
 {
@@ -35,6 +36,7 @@ class ListeYear extends Component
         //         return redirect()->back()->with('Erreur','Tous les conges restants sont restitués');
         //     }
 
+        // dd(Auth::user()->id_employe);
         $totalPaid = 0;
         $infoYear = YearTable::where('active','1')->first();
         $year_info = $infoYear->annee_en_cours;
@@ -42,21 +44,25 @@ class ListeYear extends Component
         if($year_info == $currentYear)
         {
             $paramconge = ParamTypeConge::where('id_yeartable',$infoYear->id)->first();
-            $globalpermission = PermissionConge::where('id_param_type_conge',$paramconge->id)->get();
+            $globalpermission = PermissionConge::where('id_param_type_conge',$paramconge->id)
+                                                ->where('id_employe', Auth::user()->id_employe)->get();
 
             foreach($globalpermission  as $global)
             {
-                $result = $global->employe()->where('id', $global->id)->get();
-                dd($result);
-                // $employe = Employe::where('id', $global->id_employe)->get();
+                // $employe = Employe::where('id', $global->id_employe)->first();
 
-                // $totalPaid = $global->nombre_jours_pris + $totalPaid;
 
-                // foreach ($employe as $employe) {
+                $totalPaid = $global->nombre_jours_pris + $totalPaid;
 
-                //     $employe->update(['nombre_conge_program' => $employe->nombre_conge_program + $totalPaid]);
-                // }
+                // $totalRest = $paramconge->nombre_de_jour_reserve - $totalPaid;
+
+
+                // $employe->update(['nombre_conge_program'=> $totalRest + $employe->nombre_conge_program]);
+
             }
+            dd($totalPaid);
+            toastr()->success('Bravo, Tous les congés sont restitués');
+            return redirect()->back();
         }
     }
     public function render()
