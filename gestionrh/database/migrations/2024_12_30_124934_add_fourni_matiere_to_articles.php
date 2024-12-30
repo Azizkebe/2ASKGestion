41a2bf105c48a01e96ed090dcf4c4e3e160c7887
Azizkebe <kebeziza123@gmail.com>
2024-12-30 12:52:00 +0000

    add id_matiere and id_fournisseur to article

---
+++ gestionrh/database/migrations/2024_12_30_124934_add_fourni_matiere_to_articles.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('articles', function (Blueprint $table) {
+            $table->string('id_matiere')->after('name_article')->nullable();
+            $table->string('id_fournisseur')->after('id_group')->nullable();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('articles', function (Blueprint $table) {
+            //
+        });
+    }
+};
